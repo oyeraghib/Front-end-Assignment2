be9b650eb76f019e26377336c68ec9e11aad3839
oyeraghib <raghib.khan76@gmail.com>
2022-03-16 23:02:44 +0530

    add navigation

--- app/src/main/java/com/example/front_endassignment2/MainActivity.kt
+++ app/src/main/java/com/example/front_endassignment2/MainActivity.kt
@@ -1,11 +1,27 @@
 package com.example.front_endassignment2
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.front_endassignment2.databinding.ActivityMainBinding
+import com.example.front_endassignment2.fragments.BottomSheetFragment
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
+
+        val bottomSheet = BottomSheetFragment()
+
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+//        binding.navigation.setOnClickListener{
+//            when(R.id.profile){
+//                bottomSheet.show(supportFragmentManager, "BottomSheetDialog")
+//            }
+//        }
     }
 }
