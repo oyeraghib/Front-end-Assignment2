a4eae6004af995f27d01119cc424e0e9938d0ad4
oyeraghib <raghib.khan76@gmail.com>
2022-03-17 01:43:51 +0530

    profile modal bottom sheet working

--- app/src/main/java/com/example/front_endassignment2/MainActivity.kt
+++ app/src/main/java/com/example/front_endassignment2/MainActivity.kt
@@ -8,12 +8,12 @@ import androidx.navigation.findNavController
 import androidx.navigation.ui.setupWithNavController
 import com.example.front_endassignment2.databinding.ActivityMainBinding
 import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.google.android.material.bottomsheet.BottomSheetDialog
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
-    val bottomSheetDialogFragment = BottomSheetDialogFragment()
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -31,13 +31,5 @@ class MainActivity : AppCompatActivity() {
         val navController = findNavController(R.id.nav_host_fragment_activity_main)
 
         navView.setupWithNavController(navController)
-    }
-
-    fun onSelectProfile() {
-        binding.bottomNavigation.setOnItemSelectedListener {
-            when(it.itemId){
-                R.id.profile -> bottomSheetDialogFragment.show()
-            }
-        }
     }
 }
